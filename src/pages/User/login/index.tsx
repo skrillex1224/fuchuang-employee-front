87330ab81bd4bc76b78ea7c12a0fcf8131e61d2a
572299072 <572299072@qq.com>
2021-03-04 16:12:57 +0800

    SNAPSHOT-1.0.0

---
+++ src/pages/User/login/index.tsx
@@ -0,0 +1,366 @@
+import {
+  AlipayCircleOutlined,
+  LockOutlined,
+  MailOutlined,
+  MobileOutlined,
+  TaobaoCircleOutlined,
+  UserOutlined,
+  WeiboCircleOutlined,
+} from '@ant-design/icons';
+import {Alert, Space, message, Tabs, Drawer, Button} from 'antd';
+import React, { useState } from 'react';
+import ProForm, { ProFormCaptcha, ProFormCheckbox, ProFormText } from '@ant-design/pro-form';
+import { useIntl, connect, FormattedMessage } from 'umi';
+import type { Dispatch } from 'umi';
+import type { StateType } from '@/models/login';
+import type { LoginParamsType } from '@/services/login';
+import type { ConnectState } from '@/models/connect';
+import RegisterDrawer from './Drawer/index'
+
+import styles from './index.less';
+import {ArrowRightOutlined, DownloadOutlined, RightCircleOutlined, RightOutlined} from "@ant-design/icons/lib";
+
+export type LoginProps = {
+  dispatch: Dispatch;
+  userLogin: StateType;
+  submitting?: boolean;
+};
+
+const LoginMessage: React.FC<{
+  content: string;
+}> = ({ content }) => (
+  <Alert
+    style={{
+      marginBottom: 24,
+    }}
+    message={content}
+    type="error"
+    showIcon
+  />
+);
+
+const Login: React.FC<LoginProps> = (props) => {
+
+
+  const { userLogin = {}, submitting } = props;
+  const { status, type: loginType } = userLogin;
+  const [type, setType] = useState<string>('employee');
+  const [visible, setVisible] = useState(false);
+  // 切换登录方式
+  const [isUser,setIsUser] = useState(true);
+
+
+  const intl = useIntl();
+
+  const handleSubmit = (values: LoginParamsType) => {
+    const { dispatch } = props;
+    dispatch({
+      type: 'login/login',
+      payload: { ...values, type },
+    });
+  };
+
+  const handleRegister = ()=>{
+     setVisible(true);
+  }
+
+  return (
+    <div className={styles.main}>
+      <ProForm
+        initialValues={{
+          autoLogin: true,
+        }}
+        submitter={{
+          render: (_, dom) => dom.pop(),
+          submitButtonProps: {
+            loading: submitting,
+            size: 'large',
+            style: {
+              width: '100%',
+            },
+          },
+        }}
+        onFinish={(values) => {
+          handleSubmit(values as LoginParamsType);
+          return Promise.resolve();
+        }}
+      >
+        {
+          isUser ? (
+              <Tabs activeKey={type} onChange={setType}>
+                <Tabs.TabPane
+                  key="employee"
+                  tab={intl.formatMessage({
+                    id: 'pages.login.accountLogin.tab',
+                    defaultMessage: '应聘者登录',
+                  })}
+                />
+                <Tabs.TabPane
+                  key="enterprise"
+                  tab={intl.formatMessage({
+                    id: 'pages.login.accountLogin.tab2',
+                    defaultMessage: '企业登录',
+                  })}
+                />
+              </Tabs>
+          ) : (
+            <Tabs activeKey={type} onChange={setType}>
+              <Tabs.TabPane
+                key="hr"
+                tab={intl.formatMessage({
+                  id: 'pages.login.accountLogin.tab3',
+                  defaultMessage: 'HR登录',
+                })}
+              />
+
+              <Tabs.TabPane
+                key="admin"
+                tab={intl.formatMessage({
+                  id: 'pages.login.accountLogin.tab4',
+                  defaultMessage: '管理员登录',
+                })}
+              />
+            </Tabs>
+          )
+        }
+
+
+        {status === 'error' && loginType === 'account' && !submitting && (
+          <LoginMessage
+            content={intl.formatMessage({
+              id: 'pages.login.accountLogin.errorMessage',
+              defaultMessage: '账户或密码错误（admin/ant.design)',
+            })}
+          />
+        )}
+        {type === 'employee' && (
+          <>
+            <ProFormText
+              name="userName"
+              fieldProps={{
+                size: 'large',
+                prefix: <UserOutlined className={styles.prefixIcon} />,
+              }}
+              placeholder={intl.formatMessage({
+                id: 'pages.login.username.placeholder',
+                defaultMessage: '用户名: 请输入您的用户名',
+              })}
+              rules={[
+                {
+                  required: true,
+                  message: (
+                    <FormattedMessage
+                      id="pages.login.username.required"
+                      defaultMessage="请输入用户名!"
+                    />
+                  ),
+                },
+              ]}
+            />
+            <ProFormText.Password
+              name="password"
+              fieldProps={{
+                size: 'large',
+                prefix: <LockOutlined className={styles.prefixIcon} />,
+              }}
+              placeholder={intl.formatMessage({
+                id: 'pages.login.password.placeholder',
+                defaultMessage: '密码...',
+              })}
+              rules={[
+                {
+                  required: true,
+                  message: (
+                    <FormattedMessage
+                      id="pages.login.password.required"
+                      defaultMessage="请输入密码！"
+                    />
+                  ),
+                },
+              ]}
+            />
+          </>
+        )}
+
+        {status === 'error' && loginType === 'mobile' && !submitting && (
+          <LoginMessage content="验证码错误" />
+        )}
+        {type === 'enterprise' && (
+          <>
+            <ProFormText
+              name="userName"
+              fieldProps={{
+                size: 'large',
+                prefix: <UserOutlined className={styles.prefixIcon} />,
+              }}
+              placeholder={intl.formatMessage({
+                id: 'pages.login.username.placeholder',
+                defaultMessage: '用户名: 请输入您的用户名',
+              })}
+              rules={[
+                {
+                  required: true,
+                  message: (
+                    <FormattedMessage
+                      id="pages.login.username.required"
+                      defaultMessage="请输入用户名!"
+                    />
+                  ),
+                },
+              ]}
+            />
+            <ProFormText.Password
+              name="password"
+              fieldProps={{
+                size: 'large',
+                prefix: <LockOutlined className={styles.prefixIcon} />,
+              }}
+              placeholder={intl.formatMessage({
+                id: 'pages.login.password.placeholder',
+                defaultMessage: '密码...',
+              })}
+              rules={[
+                {
+                  required: true,
+                  message: (
+                    <FormattedMessage
+                      id="pages.login.password.required"
+                      defaultMessage="请输入密码！"
+                    />
+                  ),
+                },
+              ]}
+            />
+          </>
+        )}
+        {type === 'hr' && (
+          <>
+            <ProFormText
+              name="userName"
+              fieldProps={{
+                size: 'large',
+                prefix: <UserOutlined className={styles.prefixIcon} />,
+              }}
+              placeholder={intl.formatMessage({
+                id: 'pages.login.username.placeholder',
+                defaultMessage: '用户名: 请输入您的用户名',
+              })}
+              rules={[
+                {
+                  required: true,
+                  message: (
+                    <FormattedMessage
+                      id="pages.login.username.required"
+                      defaultMessage="请输入用户名!"
+                    />
+                  ),
+                },
+              ]}
+            />
+            <ProFormText.Password
+              name="password"
+              fieldProps={{
+                size: 'large',
+                prefix: <LockOutlined className={styles.prefixIcon} />,
+              }}
+              placeholder={intl.formatMessage({
+                id: 'pages.login.password.placeholder',
+                defaultMessage: '密码...',
+              })}
+              rules={[
+                {
+                  required: true,
+                  message: (
+                    <FormattedMessage
+                      id="pages.login.password.required"
+                      defaultMessage="请输入密码！"
+                    />
+                  ),
+                },
+              ]}
+            />
+          </>
+        )}
+
+        {type === 'admin' && (
+          <>
+            <ProFormText
+              name="userName"
+              fieldProps={{
+                size: 'large',
+                prefix: <UserOutlined className={styles.prefixIcon} />,
+              }}
+              placeholder={intl.formatMessage({
+                id: 'pages.login.username.placeholder',
+                defaultMessage: '用户名: 请输入您的用户名',
+              })}
+              rules={[
+                {
+                  required: true,
+                  message: (
+                    <FormattedMessage
+                      id="pages.login.username.required"
+                      defaultMessage="请输入用户名!"
+                    />
+                  ),
+                },
+              ]}
+            />
+            <ProFormText.Password
+              name="password"
+              fieldProps={{
+                size: 'large',
+                prefix: <LockOutlined className={styles.prefixIcon} />,
+              }}
+              placeholder={intl.formatMessage({
+                id: 'pages.login.password.placeholder',
+                defaultMessage: '密码...',
+              })}
+              rules={[
+                {
+                  required: true,
+                  message: (
+                    <FormattedMessage
+                      id="pages.login.password.required"
+                      defaultMessage="请输入密码！"
+                    />
+                  ),
+                },
+              ]}
+            />
+          </>
+        )}
+
+        <div
+          style={{
+            marginBottom: 24,
+          }}
+        >
+          <a onClick={()=>{
+            console.log('before: ',isUser)
+            setIsUser(!isUser)
+            setType(  !isUser ? 'employee' :  'hr')
+            console.log(isUser);
+          }} >{isUser ? '前往HR登录' : '前往用户登录'} <RightCircleOutlined /></a>
+          <a
+            style={{
+              float: 'right',
+            }} onClick={handleRegister}
+          >
+            <div >{{'employee':'求职者注册?','enterprise':'企业注册?','admin':'管理员申请?','hr':'HR申请?'}[type]}</div>
+          </a>
+        </div>
+      </ProForm>
+
+
+      <RegisterDrawer visible={visible} setVisible={setVisible} type={type}/>
+    </div>
+
+  );
+};
+
+export default connect(({ login, loading }: ConnectState) => ({
+  userLogin: login,
+  submitting: loading.effects['login/login'],
+}))(Login);
+
