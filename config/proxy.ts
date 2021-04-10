202f6f97e86c7489d1be2d9107524bf01904d93d
skrillex1224 <572299072@qq.com>
2021-04-10 14:43:53 +0800

    RELEASE-1.0.0
    复核中心

--- config/proxy.ts
+++ config/proxy.ts
@@ -8,7 +8,7 @@
 export default {
   dev: {
     '/api/': {
-      target: 'http://localhost:8080',
+      target: 'http://10.1.255.47:8080',
       changeOrigin: true,
       pathRewrite: { '^/api/': '/' },
     },
